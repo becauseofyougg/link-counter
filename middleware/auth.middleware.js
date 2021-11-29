9a1d5df0baf5e2390390e26b6cc3c8052d81f01a
becauseofyougg <87302450+becauseofyougg@users.noreply.github.com>
2021-11-29 17:35:05 +0300

    er

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const config = requiiire('config')
+const config = require('config')
 
 module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS'){
